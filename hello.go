1334f4c84bb0241c27fedc56d6bd83455aac6094
Ikanant <jonathan.hdez92@gmail.com>
2016-04-27 01:00:00 -0500

    Add simple POINTER case (similar to C)

--- hello.go
+++ hello.go
@@ -8,7 +8,10 @@ func main(){
   firstName := "Jonathan"
   lastName := "Hernandez"
   age := 23
+  module := &age
 
   fmt.Println("My name is: ", firstName, " ", lastName)
-  fmt.Print("I am: ", age, " years old")
+  fmt.Println("I am: ", age, " years old\n\n")
+
+  fmt.Println("The AGE variable memory address is: ", module, " and its value is: ", *module)
 }
