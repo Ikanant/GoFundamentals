c592476173b0efa502aa0470dadadbeb9dc32475
Ikanant <jonathan.hdez92@gmail.com>
2016-04-28 18:31:05 -0400

    Simple IF statement

---
+++ conditionals.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+
+)
+
+func main(){
+  a := 123
+  b := 532
+
+  if a<b {
+    print("A was less than B\n")
+  } else if b>a {
+    print("B was less than A\n")
+  } else {
+    print("I guess they were equal\n")
+  }
+}
