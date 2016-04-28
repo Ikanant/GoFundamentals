5de3c92efd5b978a45d4a8de13c2797b507a34be
Ikanant <jonathan.hdez92@gmail.com>
2016-04-28 19:12:25 -0400

    Added simple switch statement

--- conditionals.go
+++ conditionals.go
@@ -15,4 +15,10 @@ func main(){
   } else {
     print("I guess they were equal\n")
   }
+
+  switch a {
+    case 0: print("A is ZERO\n")
+    case 1: print("A is ONE\n")
+    default: print("A was not ONE or ZERO\n")
+  }
 }
