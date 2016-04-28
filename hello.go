c86881ec5a524415b6022c053ae2d703bb82fd35
Ikanant <jonathan.hdez92@gmail.com>
2016-04-28 17:30:54 -0400

    Simple case of passing by REFERENCE

--- hello.go
+++ hello.go
@@ -11,7 +11,14 @@ func main(){
   module := &age
 
   fmt.Println("My name is: ", firstName, " ", lastName)
-  fmt.Println("I am: ", age, " years old\n\n")
+  fmt.Println("I am: ", age, " years old\n")
 
   fmt.Println("The AGE variable memory address is: ", module, " and its value is: ", *module)
+
+  changeFirstName(&firstName)
+  fmt.Println("\nChanged name: ", firstName)
+}
+
+func changeFirstName(firstName *string) {
+  *firstName = "Pepe"
 }
