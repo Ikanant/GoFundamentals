076df072b57e415ce83a8cc57813d5ae15879470
Ikanant <jonathan.hdez92@gmail.com>
2016-04-28 19:34:21 -0400

    Added err handler for os.Open('randomfilethatdoesnotexists.txt')

--- conditionals.go
+++ conditionals.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-
+  "math/rand"
+  "time"
+  "os"
+  "fmt"
 )
 
 func main(){
@@ -16,9 +19,19 @@ func main(){
     print("I guess they were equal\n")
   }
 
-  switch a {
-    case 0: print("A is ZERO\n")
-    case 1: print("A is ONE\n")
-    default: print("A was not ONE or ZERO\n")
+  switch tmpNum := random(); tmpNum {
+    case 0,2,4,6,8: print("Even number ", tmpNum, "\n")
+    case 1,3,5,7,9: print("Odd number ", tmpNum, "\n")
+    default: print("GODZILLAAA\n")
   }
+
+  _, err := os.Open("c:\\randomfilethatdoesnotexists.txt")
+  if err != nil {
+    fmt.Print("Error found: ", err)
+  }
+}
+
+func random() int {
+  rand.Seed(time.Now().Unix())
+  return rand.Intn(10)
 }
