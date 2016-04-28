94709d421f0318706b53aa3df357aa05b38f07e3
Ikanant <jonathan.hdez92@gmail.com>
2016-04-28 17:37:12 -0400

    Cleared and added simple Global Variable example for Windows

--- hello.go
+++ hello.go
@@ -2,23 +2,11 @@ package main
 
 import (
   "fmt"
+  "os"
 )
 
 func main(){
-  firstName := "Jonathan"
-  lastName := "Hernandez"
-  age := 23
-  module := &age
+  loggedName := os.Getenv("USERNAME")
 
-  fmt.Println("My name is: ", firstName, " ", lastName)
-  fmt.Println("I am: ", age, " years old\n")
-
-  fmt.Println("The AGE variable memory address is: ", module, " and its value is: ", *module)
-
-  changeFirstName(&firstName)
-  fmt.Println("\nChanged name: ", firstName)
-}
-
-func changeFirstName(firstName *string) {
-  *firstName = "Pepe"
+  fmt.Println(loggedName)
 }
