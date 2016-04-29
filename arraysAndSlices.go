e40be263bf5c2cc5bad271a3a260d09507b96788
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-04-29 16:12:27 -0400

    Different way to start a slice....and create a slice of slice

--- arraysAndSlices.go
+++ arraysAndSlices.go
@@ -1,14 +1,16 @@
 package main
 
-import(
-  "fmt"
+import (
+	"fmt"
 )
 
-func main(){
-  //make will need (<type>, <len>, <cap>)
+func main() {
+	//make will need (<type>, <len>, <cap>)
 
-  mySlice := make([]string, 5, 10)
-  fmt.Println("My slice length is: ", len(mySlice))
-  fmt.Println("My slice capacity is: ", cap(mySlice))
+	mySlice := []int{1, 2, 3, 4, 5}
+	sliceOfSlice := mySlice[0:3]
+
+	fmt.Println(mySlice)
+	fmt.Println(sliceOfSlice)
 
 }
