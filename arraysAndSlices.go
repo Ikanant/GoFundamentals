1b2f3e5fae8b817d6fb4f45f72283c3f8b4da302
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-04-29 16:18:50 -0400

    added append example...with Capacity check

--- arraysAndSlices.go
+++ arraysAndSlices.go
@@ -13,4 +13,11 @@ func main() {
 	fmt.Println(mySlice)
 	fmt.Println(sliceOfSlice)
 
+	myNewSlice := make([]int, 2, 4)
+
+	for i := 0; i < 20; i++ {
+		myNewSlice = append(myNewSlice, i)
+		fmt.Println("The capacity of my new Slice is: ", cap(myNewSlice))
+	}
+	fmt.Println(sliceOfSlice)
 }
